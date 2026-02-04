8ee6692c08dc1e605a8d2fa4aa9c5a5d302385aa
Nikhil Seshadri <nikhilseshadri2002@gmail.com>
2026-02-03 19:00:04 -0500

    Implemented Newton's Method calculation

--- openms/gwf/ga_mband.py
+++ openms/gwf/ga_mband.py
@@ -1,20 +1,19 @@
 import numpy as np
 import scipy.linalg
-from pyscf import lib
+from sys import argv
 
 from pdb import set_trace as st
 
 # matrix indices are given by (I, alpha) where I denotes the site, alpha the local state
 # The ordering of matrix indices is given by (0,1), ... , (0, M), (1,0), ..., (1,M), ..., (N,M)
 
-class GASCF(lib.StreamObject):
-    def __init__(self, h1e, eri, N, Ne, embedding_cutoff=1e-14):
+class GASCF:
+    def __init__(self, h1e, eri, N, Ne):
         # sanity check inputs
         if (Ne > h1e.shape[0]):
             raise ValueError
         self.N = N
         self.Ne = Ne
-        self._cutoff = embedding_cutoff
 
         h1e_shape = h1e.shape
         if (len(h1e_shape) != 2):
@@ -54,30 +53,6 @@ class GASCF(lib.StreamObject):
             U[I] = eri[idx, idx, idx, idx]
             self.U = U
 
-        # TODO: include nonlocal interactions and corresponding renormalizations
-
-        # initialize renormalizations and Lagrange multipliers
-        # how to get initial guess?
-        self.R = np.broadcast_to(np.eye(M), (N, M, M)).copy()
-
-        # everything is done in terms of
-        # \ket{\Psi_0^e} given by self.mo_occ[I]
-        # $\ket{\Psi_I}$ given by self.phi[I]
-        self.mo_energy = np.zeros((N*M))
-        self.mo_coeff = np.zeros((N*M, N*M))
-
-        # initialize all phi to uniformly id on each block (unentangled)
-        phi = np.zeros(4**M)
-        for Gamma in range(2**M):
-            phi[Gamma*(2**M) + Gamma] = 1
-
-        self.phi = np.zeros((N, 4**M))
-        for I in range(N):
-            self.phi[I] = phi
-
-        # self._C is the correlation matrix
-        self._C = np.zeros((N, N, M, M))
-
     def _get_ht(self, I):
         return self.h[I]
     
@@ -93,106 +68,9 @@ class GASCF(lib.StreamObject):
             for state in range(2**M):
                 if (state & (1 << i)):
                     C[i, state & ~(1 << i), state] = (-1)**(bin(state >> (i+1)).count('1'))
-        return C
+        return C           
     
-    def _update_renormalizations(self):
-        for I in range(self.N):
-            # get local state and correlation
-            M = self.M
-            C = self._get_annahilation_operators(M)
-            Delta = self._C[I, I]
-            B = scipy.linalg.sqrtm(np.linalg.inv(Delta @ (np.eye(M) - Delta)))
-            phi = self.phi[I]
-
-            # compute R
-            opmat = np.zeros((M, M))
-            for alpha in range(M):
-                for b in range(M):
-                    opmat[alpha, b] = phi.conj().T @ (np.kron(C[alpha].T, np.eye(2**M)) @ np.kron(np.eye(2**M), C[b].T)) @ phi
-            self.R[I] = opmat @ B
-            
-    
-    def _update_solve_qp(self):
-        N = self.N
-        M = self.M
-
-        # construct qp Hamiltonian as an N*M x N*M matrix
-        teff = np.zeros((N*M, N*M))
-        for I in range (N):
-            for J in range(N):
-                teff_IJ = self.R[I].T @ self._get_tt(I, J) @ self.R[J].conj()
-                for a in range(M):
-                    for b in range(M):
-                        teff[I*M + a, J*M + b] = teff_IJ[a, b]
-        Hqp = teff
-
-        # missing logic for lambda
-
-        # diagonalize Hqp and calculate correlation matrix
-        self.mo_energy, self.mo_coeff = np.linalg.eigh(Hqp)
-        occ = [(i < self.Ne) for i in range(N*M)]
-        C = self.mo_coeff[:, occ].conj() @ self.mo_coeff[:, occ].T
-        for I in range(N):
-            for J in range(N):
-                self._C[I, J] = C[slice(I*M, (I+1)*M), slice(J*M, (J+1)*M)]
-        
-    def get_phi_matrix(self, phi):
-        M = self.M
-        matrix = np.zeros((2**M, 2**M))
-        for Gamma in range(2**M):
-            for n in range(2**M):
-                matrix[Gamma, n] = phi[Gamma*(2**M) + n]
-        return matrix
-    
-    def _update_solve_eb(self):
-        N = self.N
-        for I in range(N):
-            M = self.M
-
-            # get coefficients for embedding Hamiltonian
-            h = self._get_ht(I)
-            U = self._get_U(I)
-            Delta = self._C[I, I]
-            A = scipy.linalg.sqrtm(Delta @ (np.eye(M) - Delta))
-            B = np.linalg.inv(A)
-
-            # calculate Lagrange multipliers
-            D = sum((self._get_tt(I, J) @ self.R[J].conj() @ self._C[I, J].T @ B.T) for J in range(N))
-
-            C = self._get_annahilation_operators(M)
-            
-            # construct local Hamiltonian
-            Hloc = sum((h[a, b] * C[a].T @ C[b]) for a in range(M) for b in range(M))
-            Hloc += sum(U[a, b, c, d] * C[a].T @ C[b].T @ C[c] @ C[d] for a in range(M) for b in range(M) for c in range(M) for d in range(M))
-            
-            # construct lambda part of the Hamiltonian
-            HL = np.zeros((2**M, 2**M))
-            for a in range(M):
-                for b in range(M):
-                    E = np.zeros((M, M))
-                    E[a, b] = 1
-                    H = (E @ (np.eye(M) - Delta)) - (Delta @ E)
-                    Z = scipy.linalg.solve_continuous_lyapunov(A, H)
-                    LC = -np.trace(D.T @ self.R[I] @ Z)
-                    HL += LC * C[b].T @ C[a]
-            HL = np.kron(np.eye(2**M), HL)
-            
-            # construct Lagrange multiplier part of the Hamiltonian
-            HM = sum(D[a, b] * np.kron(C[a].T, np.eye(2**M)) @ np.kron(np.eye(2**M), C[b].T) for a in range(M) for b in range(M)) 
-            
-            # construct and solve embedding Hamiltonian
-            Hemb = np.kron(Hloc, np.eye(2**M)) + (HM + HM.conj().T) + (HL + HL.conj().T)
-            E, eigvec = np.linalg.eigh(Hemb)
-            phiC = np.where(np.abs(eigvec) < self._cutoff, 0, eigvec)
-
-            # TODO check that groundstate is nondegenerate
-            if (E[0] == E[1]):
-                # groundstate is degenerate
-                st()
-            self.phi[I] = phiC[:,0]
-
-    
-    # phi is a vector of size N, containing embedding matrix states each of size 2**M x 2**M
+    # phi is a vector of size N, containing embedding matrix states each of size 2**M x 2**M encoded as a 4**M component vector
     # L is a vector of size N, containing matrices of size MxM
     # Lc is a vector of size N, containing matrices of size MxM
     # Delta is a vector of size N, containing Hermitian matrices of size MxM
@@ -215,19 +93,15 @@ class GASCF(lib.StreamObject):
             parts.append(Delta[i].imag.ravel())
         parts.append(np.asarray(Ec).ravel())
 
-        print("phi sizes:", [phiarr[i].size for i in range(N)])
-        print("L sizes:  ", [L[i].size for i in range(N)])
-        print("Lc sizes: ", [Lc[i].size for i in range(N)])
-        print("Delta sizes:", [Delta[i].size for i in range(N)])
-        print("Ec size:", np.asarray(Ec).ravel().size)
+        # assert all(phiarr[i].size == 4**M for i in range(N))
+        # assert all(L[i].size == M*M for i in range(N))
+        # assert all(Lc[i].size == M*M for i in range(N))
+        # assert all(Delta[i].size == M*M for i in range(N))
+        # assert np.asarray(Ec).ravel().size == N
 
-        assert all(phiarr[i].size == 4**M for i in range(N))
-        assert all(L[i].size == M*M for i in range(N))
-        assert all(Lc[i].size == M*M for i in range(N))
-        assert all(Delta[i].size == M*M for i in range(N))
-        assert np.asarray(Ec).ravel().size == N
-
-        return np.concatenate(parts)
+        result = np.concatenate(parts)
+        assert all(result.imag == 0)
+        return result.real
     
     def _unpack_vector(self, x):
         idx = 0
@@ -241,7 +115,6 @@ class GASCF(lib.StreamObject):
             Im = x[idx:(idx + size_phi)].reshape(size_phi)
             idx += size_phi
             phiarr.append(Re + 1j*Im)
-        print(f"idx = {idx}")
 
         L = []
         size_sq = M*M
@@ -251,7 +124,6 @@ class GASCF(lib.StreamObject):
             Im = x[idx:(idx + size_sq)].reshape(M, M)
             idx += size_sq
             L.append(Re + 1j*Im)
-        print(f"idx = {idx}")
         Lc = []
         for _ in range(N):
             Re = x[idx:(idx + size_sq)].reshape(M, M)
@@ -259,7 +131,6 @@ class GASCF(lib.StreamObject):
             Im = x[idx:(idx + size_sq)].reshape(M, M)
             idx += size_sq
             Lc.append(Re + 1j*Im)
-        print(f"idx = {idx}")
 
         Delta = []
         for _ in range(N):
@@ -268,14 +139,20 @@ class GASCF(lib.StreamObject):
             Im = x[idx:(idx + size_sq)].reshape(M, M)
             idx += size_sq
             Delta.append(Re + 1j*Im)
-        print(f"idx = {idx}")
 
         Ec = x[idx:(idx+N)]
         idx += N
-        print(f"idx = {idx}")
 
         assert idx == len(x), "Unpack error: leftover elements in input array"
         return phiarr, L, Lc, Delta, Ec
+    
+    def get_phi_matrix(self, phi):
+        phi_size = int(np.sqrt(phi.shape)[0])
+        matrix = np.zeros((phi_size, phi_size), dtype=phi.dtype)
+        for Gamma in range(phi_size):
+            for n in range(phi_size):
+                matrix[Gamma, n] = phi[Gamma*(phi_size) + n]
+        return matrix
 
     def _compute_renormalizations(self, phiarr, Delta):
         R = []
@@ -332,7 +209,7 @@ class GASCF(lib.StreamObject):
         return Hemb
     
     # this function assumes that the Hqp eigenenergies are sorted from lowest to highest
-    def _compute_firstorder_perturubation_energy(self, eigvals, eigmatrix, DH):
+    def _compute_firstorder_perturbation_energy(self, eigvals, eigmatrix, DH):
         # get the degenerate subspaces of Hqp
         boundaries = np.where(np.diff(eigvals) != 0)[0] + 1
         indices = np.arange(len(eigvals))
@@ -418,7 +295,7 @@ class GASCF(lib.StreamObject):
         grad_L = []
         for I in range(N):
             M = self.M
-            Lm = np.zeros((M,M))
+            Lm = np.zeros((M,M), dtype=np.complex128)
             for a in range(M):
                 for b in range(M):
                     DH = np.zeros(Hqp.shape)
@@ -429,7 +306,7 @@ class GASCF(lib.StreamObject):
         grad_Lc = []
         for I in range(N):
             M = self.M
-            Lm = np.zeros((M,M))
+            Lm = np.zeros((M,M), dtype=np.complex128)
             C = self._get_annahilation_operators(M)
             for a in range(M):
                 for b in range(M):
@@ -445,7 +322,7 @@ class GASCF(lib.StreamObject):
             Dm = -L[K] - Lc[K]
 
             # calculate renormalization based matrix
-            P = np.zeros((M,M))
+            P = np.zeros((M,M), dtype=np.complex128)
             C = self._get_annahilation_operators(M)
             for alpha in range(M):
                 for gamma in range(M):
@@ -464,7 +341,7 @@ class GASCF(lib.StreamObject):
                     Z = scipy.linalg.solve_continuous_lyapunov(A, H)
                     Y = -B @ Z @ B
                     # calculate $\frac{\partial{H_{qp}}}{\partial{\Delta^K_{yz}}}$
-                    DH = np.zeros(Hqp.shape)
+                    DH = np.zeros(Hqp.shape, dtype=np.complex128)
                     for I in range(N):
                         M1 = Y.T @ P.T @ self._get_tt(K, I) @ R[I].conj()
                         M2 = R[I].T @ self._get_tt(I, K) @ P.conj() @ Y.T
@@ -475,42 +352,49 @@ class GASCF(lib.StreamObject):
                     # calculate contribution to the full derivative
                     Dm[y, z] += self._compute_derivative_energy(mo_coeff, DH)
 
+            # The correlation matrix Delta must be Hermitian
+            # therefore the derivative wrt Delta must also be Hermitian
+            # convergence can be improved by only considering the Hermitan part of the computed gradient
+            Dm = 0.5*(Dm + Dm.conj().T)
             grad_Delta.append(Dm)
 
         # return packed vector
         return self._pack_vector(grad_phiarr, grad_L, grad_Lc, grad_Delta, grad_Ec)
 
-    def naive_kernel(self):
-        for i in range(100):
-            prev_coeff = self.mo_coeff
-            # set mo_energy, mo_coeff, C, by solving  Hqp
-            self._update_solve_qp()
-            # set phi by solving Hemb
-            self._update_solve_eb()
-
-            self._update_renormalizations()
-
-            print(f"Iteration {i+1}: diff {np.linalg.norm(prev_coeff - self.mo_coeff)}")
-            
-        st()
-
-        self._post_kernel()
-
-    def kernel(self):
+    def kernel(self, method="krylov", maxiter=None, tolerance=1e-4, verbose=True):
         # construct initial guess
         N = self.N
-        phiarr = [self.phi[I] for I in range(N)]
+
+        # initialize all phi to uniformly id on each block (unentangled)
+        phiarr = []
+        L = []
+        Lc = []
+        Delta = []
+        for I in range(N):
+            M = self.M
+            phi = np.zeros(4**M, dtype=np.complex128)
+            for Gamma in range(2**M):
+                phi[Gamma*(2**M) + Gamma] = 1
+            phiarr.append(phi)
+
+            ZM = np.zeros((M,M), dtype=np.complex128)
+            L.append(ZM.copy())
+            Lc.append(ZM.copy())
+            Delta.append((self.Ne/(N*M))*np.eye(M))
+        
         Ec = np.zeros(N)
 
-        M = self.M
-        Delta = [(self.Ne/M)*np.eye(M) for I in range(N)]
-
-        L = [np.zeros((M,M)) for I in range(N)]
-        Lc = [np.zeros((M,M)) for I in range(N)]
-
         x0 = self._pack_vector(phiarr, L, Lc, Delta, Ec)
-        
-        result = scipy.optimize.root(self._compute_gradient, x0, method="hybr" or "krylov")
+
+        options = {'fatol':tolerance}
+        if maxiter:
+            options['maxiter'] = maxiter
+        if verbose:
+            options['disp'] = True
+        result = scipy.optimize.root(self._compute_gradient, x0, method=method, options=options)
+
+        phiarr, L, Lc, Delta, Ec = self._unpack_vector(result.x)
+        print(f"Computed Ne = {sum(np.trace(Delta[I]) for I in range(N))}, self.Ne = {self.Ne}")
 
         st()
 
@@ -544,4 +428,7 @@ def get_ga_model(N=12, filling=0.5, U=2.0, t=-1.0, PBC=True):
 
 if __name__ == '__main__':
     gamf = get_ga_model(PBC=False)
-    gamf.kernel()
+    if (len(argv) == 2):
+        gamf.kernel(method=argv[1])
+    else:
+        gamf.kernel(maxiter=500)
